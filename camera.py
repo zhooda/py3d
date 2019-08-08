f04598bed6e98ae7cc59d3e123877840519c40ff
Zeeshan Hooda <zeeshanhooda@icloud.com>
2019-08-08 00:53:10 -0600

    Now you go in the direction your looking!

--- camera.py
+++ camera.py
@@ -20,10 +20,20 @@ class Cam:
 		if key[pygame.K_q]: self.pos[1] -= s
 		if key[pygame.K_e]: self.pos[1] += s
 
-		if key[pygame.K_w]: self.pos[2] += s
-		if key[pygame.K_s]: self.pos[2] -= s
-		if key[pygame.K_a]: self.pos[0] -= s
-		if key[pygame.K_d]: self.pos[0] += s
+		x,y = s*math.sin(self.rot[1]), s*math.cos(self.rot[1])
+
+		if key[pygame.K_w]: 
+			self.pos[0] += x
+			self.pos[2] += y
+		if key[pygame.K_s]: 
+			self.pos[0] -= x
+			self.pos[2] -= y
+		if key[pygame.K_a]: 
+			self.pos[0] -= y
+			self.pos[2] += x
+		if key[pygame.K_d]: 
+			self.pos[0] += y
+			self.pos[2] -= x
 
 
 def rotate2d(pos, rad):
